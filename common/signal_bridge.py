0a934476ff984763e16e123ba9bd2d9e87b24bff
bkmcm9205 <mcmillen.b@gmail.com>
2025-10-12 21:25:15 -0400

    Create signal_bridge.py

---
+++ common/signal_bridge.py
@@ -0,0 +1,2 @@
+from adapters.broker_alpaca import send_signal as send_to_broker, close_position, close_all_positions, list_positions, get_account_equity
+__all__ = ["send_to_broker", "close_position", "close_all_positions", "list_positions", "get_account_equity"]
